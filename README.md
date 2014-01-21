741b112b685937e4ea0d54e9411339a51656c58e
Mahemoff <michael@mahemoff.com>
2014-01-21 20:40:19 +0000

    Better automation process

--- README.md
+++ README.md
@@ -2,3 +2,10 @@ TestFeeds
 =========
 
 Some test RSS feeds
+
+Installing
+==========
+
+1. git clone
+2. bin/make.sh
+3. ensure public web folder is pointing at web/
--- make.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-for file in *.rss ; do
-  echo $file
-  sed -i.bak 's/%%HOST%%/http:\/\/testdata.player.fm/g' $file
-done
-
-bakdir=/tmp/rssbak$$
-mkdir $bakdir
-mv -f *.bak $bakdir
