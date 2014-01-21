a26163024df3949e87119fbd1e86cfc375368915
Mahemoff <michael@mahemoff.com>
2014-01-21 15:58:23 +0000

    hardcode host in make

--- make.sh
+++ make.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
-host='http://testdata.player.fm'
 for file in *.rss ; do
   echo $file
-  sed -i.bak 's/%%HOST%%/$host/g' $file
+  sed -i.bak 's/%%HOST%%/http:\/\/testdata.player.fm/g' $file
 done
