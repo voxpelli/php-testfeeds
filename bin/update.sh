a6d089b63d0768aacb5f5e0e2f19a842cba9b292
Mahemoff <michael@mahemoff.com>
2014-04-08 19:58:40 +0200

    introduce random class

--- bin/update.sh
+++ bin/update.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # in bash_profile, use export testdata_dest=user@testdata.player.fm
-ssh -n $testdata_dest 'cd testdata; git pull; bin/media.sh; exit'
+#ssh -n $testdata_dest 'cd testdata; git pull; bin/media.sh; exit'
+ssh -n $testdata_dest 'cd testdata; git pull; exit'
---
+++ web/random.php
@@ -0,0 +1,24 @@
+<?
+# Deterministic random generator
+# http://www.sitepoint.com/php-random-number-generator/
+
+class Random {
+
+  // random seed
+  private static $RSeed = 0;
+
+  // set seed
+  public static function seed($s = 0) {
+    self::$RSeed = abs(intval($s)) % 9999999 + 1;
+    self::num();
+  }
+
+  // generate random number
+  public static function num($min = 0, $max = 9999999) {
+    if (self::$RSeed == 0) self::seed(mt_rand());
+    self::$RSeed = (self::$RSeed * 125) % 2796203;
+    return self::$RSeed % ($max - $min + 1) + $min;
+  }
+
+}
+?>
