0b382a36ff61e8423a374f60e78fe72702fce3b5
Mahemoff <michael@mahemoff.com>
2014-04-08 20:06:39 +0200

    simplify random

--- web/random.php
+++ web/random.php
@@ -4,18 +4,13 @@
 
 class Random {
 
-  // random seed
-  private static $RSeed = 0;
+  private static $RSeed;
 
-  // set seed
   public static function seed($s = 0) {
     self::$RSeed = abs(intval($s)) % 9999999 + 1;
-    self::num();
   }
 
-  // generate random number
   public static function num($min = 0, $max = 9999999) {
-    if (self::$RSeed == 0) self::seed(mt_rand());
     self::$RSeed = (self::$RSeed * 125) % 2796203;
     return self::$RSeed % ($max - $min + 1) + $min;
   }
